69f000f82f9b136d97bdbdaf5aca75137cda618d
Joseph Pasque <pasquej@wustl.edu>
2021-10-17 13:27:54 -0500

    wipe node_modules

--- browser/node_modules/entropy-td-core/lib/friendly/player.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class PlayerState {
-    money: number;
-    constructor(config: PlayerStateConfiguration);
-    canAfford(cost: number): boolean;
-    makePurchase(cost: number): void;
-}
-export interface PlayerStateConfiguration {
-    startingMoney: number;
-}
--- browser/node_modules/entropy-td-core/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { GameOrchestrator } from "./orchestrator";
-export { GameBoard } from "./game_board";
